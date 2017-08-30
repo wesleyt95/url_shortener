aff85c142c9fc1acf7811b4b6388d177b47c2e3e
wesleyt95 <wesley.bond@icloud.com>
2017-08-30 06:30:44 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :urls
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get “/:short_url”, to: "urls#show"
+  get "/:short_url", to: "urls#show"
 end
